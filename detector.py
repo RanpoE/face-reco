21339b363ca34719b8d0a6af5cadf605969759c7
reborn <r4yeer@gmail.com>
2024-02-17 06:55:38 +0800

    add args and validate function

--- detector.py
+++ detector.py
@@ -5,6 +5,7 @@ import pickle
 import face_recognition
 from collections import Counter
 from PIL import Image, ImageDraw, ImageFont
+import argparse
 
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
@@ -15,6 +16,26 @@ TEXT_COLOR = "white"
 Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
 Path("validation").mkdir(exist_ok=True)
+
+parser = argparse.ArgumentParser(description="Recognize faces in an image")
+parser.add_argument("--train", action="store_true", help="Train on input data")
+parser.add_argument(
+    "--validate", action="store_true", help="Validate trained model"
+)
+parser.add_argument(
+    "--test", action="store_true", help="Test the model with an unknown image"
+)
+parser.add_argument(
+    "-m",
+    action="store",
+    default="hog",
+    choices=["hog", "cnn"],
+    help="Which model to use for training: hog (CPU), cnn (GPU)",
+)
+parser.add_argument(
+    "-f", action="store", help="Path to an image with an unknown face"
+)
+args = parser.parse_args()
 
 def _display_face(draw, bounding_box, name):
     top, right, bottom, left = bounding_box
@@ -100,9 +121,23 @@ def recognize_faces(
     pillow_image.show()
 
 
+def validate(model: str = "hog"):
+    for filepath in Path("validation").rglob("*"):
+        if filepath.is_file():
+            recognize_faces(
+                image_location=str(filepath.absolute()), model=model
+            )
     
-recognize_faces("test.jpg")
+# recognize_faces("test.jpg")
 
 
 # encode_known_faces()
+
+if __name__ == "__main__":
+    if args.train:
+        encode_known_faces(model=args.m)
+    if args.validate:
+        validate(model=args.m)
+    if args.test:
+        recognize_faces(image_location=args.f, model=args.m)
 
